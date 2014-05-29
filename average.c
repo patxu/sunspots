c1bd28c3cdefc0ddbb57dc826c593c20b5013cb4
Patrick Xu <xu.17@dartmouth.edu>
2014-05-29 11:37:48 -0400

    added function descriptions

--- average.c
+++ average.c
@@ -3,7 +3,9 @@
  *
  * average.c
  *
- * Description: 
+ * finds the average number of sunspots of a particular month and compares this to the
+ * average number of sunspots across all months. also informs the user how this number
+ * compares in the context of the sunspot cycle
  * 
  */
 
--- average.h
+++ average.h
@@ -1,10 +1,21 @@
 #ifndef _AVERAGE_H_
 #define _AVERAGE_H_
 
+/*
+ * finds the average number of sunspots of a particular month and compares this to the
+ * average number of sunspots across all months. also informs the user how this number
+ * compares in the context of the sunspot cycle
+ */
 void monthAverage(char* dir);
 
+/*
+ * gets user input for the month and year
+ */
 void getMonthandYear(int* month, int* year);
 
+/*
+ * gets the overall average number of sunspots per month across all months
+ */
 int getOverallAverage(char fullpath[]);
 
 #endif
